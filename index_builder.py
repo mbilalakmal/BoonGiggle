65067e5f632001b55ff4c5b1fbd91a0ee1a6b203
Bilal Akmal <bilalakmal@hotmail.com>
2020-03-08 22:37:08 +0500

    Title is not indexed. Doc_ids are stored in another dict

--- index_builder.py
+++ index_builder.py
@@ -2,42 +2,46 @@
 # This module creates an inverted index from the documents.
 #
 # The steps performed are summarized below:
-# (1) Read all the documents
-# (2) Create tokens from each document
-# (3) Preprocess (case-fold and stem) tokens
-# (4) Store each term along with document ID and position
-# (5) Store the inverted index dictionary as a binary file
+# (1) Read all the documents.
+# (2) Store Document IDs against their file names.
+# (3) Create tokens from each document.
+# (4) Preprocess (case-fold and stem) tokens.
+# (5) Store each term along with document ID and position.
+# (6) Store the inverted index dictionary as a binary file.
 #
 #
 # (C) 2020 Muhammad Bilal Akmal, 17K-3669
 # email k173669@nu.edu.pk
 # -----------------------------------------------------------
 
-import pickle           # dump and load | filing
-import re               # split | tokenizing
-import time             # time | timing the whole preprocessing
-from glob import glob   # | file names of all txt files
-
-from nltk import PorterStemmer  # stem | stemming
-
-# with open(r'resources\inverted_index', 'rb') as index_file:
-#     inverted_index_2 = pickle.load(index_file)
+import pickle           # dump  | storing to file
+import re               # split | tokenizing of documents' body
+from glob import glob   # glob  | discover names of all txt files
+from nltk import PorterStemmer  # stem  | stemming of tokens
 
 
 def read_txt_files(pathname, encoding = 'utf=8'):
     '''
     Return txt documents discovered in `pathname`.
     '''
-    documents = {}  # key = filename, value = document
+    documents = {}  # key = id, value = document
+
+    doc_ids = {}    # key = id, value = filename
 
     # discover txt files in pathname
     # and load into documents dictionary
     for file_name in glob(pathname):
-        with open(file_name, 'r', encoding=encoding) as txt_file:
-            document = txt_file.read()
-            documents[file_name] = document
 
-    return documents
+        #get speech number from file name
+        doc_id = re.search(r'\d+', file_name).group(0)
+        doc_ids[doc_id] = file_name
+
+        with open(file_name, 'r', encoding=encoding) as txt_file:
+            _ = txt_file.readline()     #discard title
+            document = txt_file.read()
+            documents[doc_id] = document
+
+    return (doc_ids, documents)
 
 
 def read_stop_words(filename):
@@ -45,7 +49,7 @@ def read_stop_words(filename):
     Return stop words present in `filename`.
     '''
     with open(filename, 'r') as txt_file:
-        stop_words = set(txt_file.read().split())
+        stop_words = set( txt_file.read().split() )
     return stop_words
 
 
@@ -53,10 +57,8 @@ def sanitize_tokens(tokens):
     '''
     Return case-folded and stemmed `tokens`.
     '''
-    # case-fold tokens
     folded_tokens = [token.lower() for token in tokens]
 
-    # stem tokens
     stemmer = PorterStemmer()
     stemmed_tokens = [stemmer.stem(token) for token in folded_tokens]
 
@@ -80,8 +82,8 @@ def build_positional_index(documents, stop_words):
 
         for position, term in enumerate(sanitized_tokens):
 
-            if term in stop_words:  # skip stop words
-                continue
+            if term in stop_words:
+                continue    #skip stop words
 
             # create term dict
             if term not in inverted_index.keys():
@@ -96,21 +98,25 @@ def build_positional_index(documents, stop_words):
     return inverted_index
 
 
-def store_inverted_index(inverted_index, filename):
+def store_python_object(python_object, filename):
     '''
-    Store `inverted_index` Python object in `filename`.
+    Store `python_object` in `filename`.
 
     If `filename` exists, it will be overwritten.
     '''
-    with open(filename, 'wb') as index_file:
-        pickle.dump(inverted_index, index_file)
+    with open(filename, 'wb') as bin_file:
+        pickle.dump(python_object, bin_file)
 
 
 if __name__ == '__main__':
 
     # Read all the documents
     pathname = r'resources\corpus\*.txt'
-    documents = read_txt_files(pathname)
+    doc_ids, documents = read_txt_files(pathname)
+
+    # Store doc_ids
+    filename = r'resources\doc_ids'
+    store_python_object(doc_ids, filename)
 
     #Read the stop words
     filename = r'resources\stopwords.txt'
@@ -121,4 +127,6 @@ if __name__ == '__main__':
 
     # Store the inverted index
     filename = r'resources\inverted_index'
-    store_inverted_index(inverted_index, filename)
+    store_python_object(inverted_index, filename)
+
+    

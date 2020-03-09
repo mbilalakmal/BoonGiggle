93fc1f665f39f12f94a3f2380586b41868654e76
Bilal Akmal <bilalakmal@hotmail.com>
2020-03-09 22:51:26 +0500

    Query recognizer (Boolean, Phrase, or Proximity)

---
+++ query_dispatcher.py
@@ -0,0 +1,25 @@
+# -----------------------------------------------------------
+# This module determines the type of query and dispatches
+# the appropriate parser.
+#
+#
+# (C) 2020 Muhammad Bilal Akmal, 17K-3669
+# email k173669@nu.edu.pk
+# -----------------------------------------------------------
+
+import re
+import boolean_parser
+import phrase_parser
+import proximity_parser
+
+def dispatch_query(query):
+
+    bool_regex = r'AND|OR|NOT'
+    prox_regex = r'/\d'
+
+    if re.search(bool_regex, query) != None:
+        return boolean_parser.retreive_documents(query)
+    elif re.search(prox_regex, query) != None:
+        return proximity_parser.retreive_documents(query)
+    else:
+        return phrase_parser.retreive_documents(query)
